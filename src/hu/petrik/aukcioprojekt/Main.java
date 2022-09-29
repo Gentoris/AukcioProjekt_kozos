8336edacd9a21e035228f9287521406a265c8dcb
Kerecsényi Márk <karatelovag@gmail.com>
2022-09-29 17:20:33 +0200

    Festmény osztály adattagok getterek es setterek konstruktor licit eljárás

--- src/hu/petrik/aukcioprojekt/Main.java
+++ src/hu/petrik/aukcioprojekt/Main.java
@@ -3,6 +3,11 @@ package hu.petrik.aukcioprojekt;
 public class Main {
 
     public static void main(String[] args) {
-
+        Festmeny festmeny = new Festmeny("Új festmény", "Gipsz Jakab", "futurizmus");
+        // TODO: konzolalkalmazás elkészítése
+        for (int i = 0; i < 100; i++) {
+            festmeny.licit();
+            System.out.println(festmeny.getLegmagasabbLicit());
+        }
     }
 }
